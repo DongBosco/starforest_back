0a504730af72e17b2701f79ecfe5517bf42a6366
DongBsoco <dk960227@hanmail.net>
2024-07-30 15:42:04 +0900

    COMMIT_WITH_BORAM

--- src/main/java/com/project/starforest/config/JacksonConfig.java
+++ src/main/java/com/project/starforest/config/JacksonConfig.java
@@ -13,9 +13,8 @@ public class JacksonConfig {
 	@Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule()); // Java 8 시간 API 모듈 등록
+        mapper.registerModule(new JavaTimeModule());
         
-     // Point 객체의 직렬화와 역직렬화 모듈 등록
         SimpleModule module = new SimpleModule();
         module.addSerializer(Point.class, new PointSerializer());
         module.addDeserializer(Point.class, new PointDeserializer());
